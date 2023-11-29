c118543ccd1b15520ee083ca473ac0dd99af899e
Miralem.Ba <miralembanovi@gmail.com>
2023-11-29 19:42:14 +0100

    push

---
+++ src/app/elements/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+@Component({
+  selector: 'pm-header',
+  standalone: true,
+  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatIconModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
