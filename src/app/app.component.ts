3c2b370dbda063fcc7c711c4a34f890164f97136
Miralem.Ba <miralembanovi@gmail.com>
2023-11-30 19:47:28 +0100

    app.component bearbeitet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import {FooterComponent} from "./elements/footer/footer.component";
 import {HeaderComponent} from "./elements/header/header.component";
 
 @Component({
   selector: 'pm-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FooterComponent, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, FooterComponent, HeaderComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {CategoriesComponent} from "./categories/categories.component";
+import {ProductsComponent} from "./products/products.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'categories', component: CategoriesComponent },
+    { path: 'products', component: ProductsComponent }
+
+];
+
