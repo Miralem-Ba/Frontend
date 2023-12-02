d512f22b612c287157ab65601b55bfdf93ca5c4d
Miralem.Ba <miralembanovi@gmail.com>
2023-12-02 10:03:08 +0100

    überarbeitet

--- src/app/elements/header/header.component.ts
+++ src/app/elements/header/header.component.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {RouterLink} from "@angular/router";
 @Component({
   selector: 'pm-header',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatIconModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
