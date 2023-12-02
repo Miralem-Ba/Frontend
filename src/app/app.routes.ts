301d782a9ccae51f1536464d5515e567aa5b6be6
Miralem.Ba <miralembanovi@gmail.com>
2023-12-02 14:10:05 +0100

    app.routes.ts bearbeitet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,35 @@
-import { Routes } from '@angular/router';
-import {CategoriesComponent} from "./categories/categories.component";
-import {ProductsComponent} from "./products/products.component";
+
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+
+import { CategoriesComponent } from "./categories/categories.component";
+import { LoginComponent } from './pages/auth/login/login.component';
+import { RegisterComponent } from './pages/auth/register/register.component';
+import {ProductsComponent} from "./pages/products/products.component";
+
 
 export const routes: Routes = [
-    { path: 'categories', component: CategoriesComponent },
-    { path: 'products', component: ProductsComponent }
-
+  {
+    path: 'categories',
+    component: CategoriesComponent
+  },
+  {
+    path: 'products',
+    component: ProductsComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
