2b56a1040c8f89e3b33b882ea1004fa57638e178
Miralem.Ba <miralembanovi@gmail.com>
2023-12-02 16:53:00 +0100

    bearbeitet

--- src/app/pages/auth/register/register.component.ts
+++ src/app/pages/auth/register/register.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from "@angular/material/select";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'pm-register',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatInputModule, MatSelectModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
