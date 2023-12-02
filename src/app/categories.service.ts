270c037ad8e831684334050757afd23ac25a0893
Miralem.Ba <miralembanovi@gmail.com>
2023-12-02 12:08:33 +0100

    category.service.ts bearbeitet

--- src/app/categories.service.ts
+++ src/app/categories.service.ts
@@ -1,14 +1,29 @@
+// category.service.ts
+
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CategoriesService {
+export class CategoryService {
+  private apiUrl = 'https://your-api-url.com';
 
   constructor(private http: HttpClient) { }
 
   getCategories() {
-    return this.http.get('/api/categories');
+    return this.http.get(this.apiUrl + '/categories');
+  }
+
+  getCategoryById(id: number) {
+    return this.http.get(this.apiUrl + `/categories/${id}`);
+  }
+
+  updateCategory(id: number, categoryData: any) {
+    return this.http.put(this.apiUrl + `/categories/${id}`, categoryData);
+  }
+
+  deleteCategory(id: number) {
+    return this.http.delete(this.apiUrl + `/categories/${id}`);
   }
 }
