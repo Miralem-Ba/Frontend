53ec80b96e6b998e3d6d51515e0e0d2ced80c905
Miralem.Ba <miralembanovi@gmail.com>
2023-12-02 14:18:41 +0100

    login.component.ts bearbeitet

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,19 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'pm-login',
   standalone: true,
-  imports: [CommonModule, FormsModule, MatInputModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, MatInputModule, ReactiveFormsModule, MatButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  fromGroup: any;
+  fromGroup = new FormGroup({
+    email: new FormControl(),
+    password: new FormControl()
+  });
 
   submit() {
-
+    console.log(this.fromGroup);
+    if(this.fromGroup.valid){
+      //
+    }
   }
 }
