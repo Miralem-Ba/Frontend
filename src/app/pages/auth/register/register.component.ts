b1b9e1fa67251ad40695d282694c249e679784d1
Miralem.Ba <miralembanovi@gmail.com>
2023-12-05 22:02:48 +0100

    register.component.ts bearbeitet

--- src/app/pages/auth/register/register.component.ts
+++ src/app/pages/auth/register/register.component.ts
@@ -1,16 +1,56 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component, OnInit} from "@angular/core";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {MatButtonModule} from "@angular/material/button";
+import {MatCheckboxModule} from "@angular/material/checkbox";
 import {MatInputModule} from "@angular/material/input";
 import {MatSelectModule} from "@angular/material/select";
-import {ReactiveFormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
+import {MatCardModule} from "@angular/material/card";
+
 
 @Component({
   selector: 'pm-register',
   standalone: true,
-  imports: [CommonModule, MatInputModule, MatSelectModule, ReactiveFormsModule],
+  imports: [CommonModule, MatInputModule, MatSelectModule, ReactiveFormsModule, MatCheckboxModule, MatButtonModule, MatCardModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+@Component({
+  selector: 'pm-register',
+  standalone: true,
+  imports: [
+    CommonModule, MatInputModule, MatSelectModule,
+    ReactiveFormsModule, MatCheckboxModule, MatButtonModule
+  ],
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
 
+export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      street: ['', Validators.required],
+      zip: ['', [Validators.required, Validators.pattern('[0-9]*')]],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      phone: ['', Validators.pattern('[0-9]+')],
+      mobilePhone: ['', Validators.pattern('[0-9]+')],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
+  }
+
+  register() {
+    if (this.registerForm.valid) {
+      console.log('Registration data:', this.registerForm.value);
+    } else {
+      console.log('Form is not valid');
+    }
+  }
 }
