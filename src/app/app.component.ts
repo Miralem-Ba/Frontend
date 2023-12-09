bf8483007c25ba3373a2c1f11194a17e15d632f3
Miralem.Ba <miralembanovi@gmail.com>
2023-12-09 12:06:06 +0100

    app.config.ts und app.component.ts bearbeitet.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {RouterLink, RouterOutlet} from '@angular/router';
+import {RouterLink, RouterModule, RouterOutlet} from '@angular/router';
 import {FooterComponent} from "./elements/footer/footer.component";
 import {HeaderComponent} from "./elements/header/header.component";
 
 @Component({
   selector: 'pm-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FooterComponent, HeaderComponent, RouterLink],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FooterComponent,
+    HeaderComponent,
+    RouterLink
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,15 @@
-import { ApplicationConfig } from '@angular/core';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptors} from "@angular/common/http";
+import {authorizationInterceptor} from "./interceptors/authorization.interceptor";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(), provideAnimations()]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([authorizationInterceptor])),
+    provideAnimations()
+  ]
 };
