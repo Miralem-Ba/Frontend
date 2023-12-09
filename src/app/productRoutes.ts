f3363aaa159714f90fe859fd3230460907fce433
Miralem.Ba <miralembanovi@gmail.com>
2023-12-09 17:04:16 +0100

    überarbeitet

--- src/app/productRoutes.ts
+++ src/app/productRoutes.ts
@@ -2,9 +2,14 @@ import {Routes} from "@angular/router";
 import {ProductsCreateComponent} from "./pages/products/products-create/products-create.component";
 import {ProductsDetailComponent} from "./pages/products/products-detail/products-detail.component";
 import {authGuard} from "./gutar/auth.guard";
+import {ProductsListComponent} from "./pages/products/products-list/products-list.component";
 
 export let productRoutes: Routes = [{
-
+  path: 'list',
+  loadComponent:() => import("./pages/products/products-list/products-list.component").then(val => val.ProductsListComponent),
+  // component: ProductsListComponent},
+},
+  {
   path: 'create',
   loadComponent: () => import("./pages/products/products-create/products-create.component").then(val => val.ProductsCreateComponent),
   // component: ProductsCreateComponent},
@@ -12,7 +17,7 @@ export let productRoutes: Routes = [{
 },
   {
   path:'detail',
-    loadComponent: () => import("./pages/products/products-detail/products-detail.component").then(val => val.ProductsDetailComponent)
+    loadComponent: () => import("./pages/products/products-detail/products-detail.component").then(val => val.ProductsDetailComponent),
     // component:ProductsDetailComponent
 
 }];
