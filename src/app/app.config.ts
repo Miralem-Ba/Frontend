ad63069b508e0ccd95137de9ea0fed12698a31f9
Miralem.Ba <miralembanovi@gmail.com>
2023-12-09 14:42:25 +0100

    authorization.interceptor.ts und app.config.ts bearbeitet

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptors} from "@angular/common/http";
+import {provideHttpClient, withInterceptors} from "@angular/common/http";
 import {authorizationInterceptor} from "./interceptors/authorization.interceptor";
 
 export const appConfig: ApplicationConfig = {
--- src/app/interceptors/authorization.interceptor.ts
+++ src/app/interceptors/authorization.interceptor.ts
@@ -1,3 +1,31 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpEvent, HttpHandler, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Route, Router } from '@angular/router';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private router: Router) {}
+
+  intercept(
+    req: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+
+    if (localStorage.getItem('ACCESS_TOKEN')) {
+
+      return  next.handle(req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${localStorage.getItem('ACCESS_TOKEN')}`
+        }
+      }));
+    }
+    return next.handle(req);
+  }
+}
+
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authorizationInterceptor: HttpInterceptorFn = (req, next) => {
