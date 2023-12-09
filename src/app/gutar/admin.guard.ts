2d9acc0f57a1250d0a3dfc8bbccbe066b97b1dea
Miralem.Ba <miralembanovi@gmail.com>
2023-12-09 13:48:02 +0100

    admin.guard.ts klasse beartbeitet

--- src/app/gutar/admin.guard.ts
+++ src/app/gutar/admin.guard.ts
@@ -1,5 +1,26 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { jwtDecode} from 'jwt-decode';
+
+interface TokenPayload {
+  roles: string[];
+}
 
 export const adminGuard: CanActivateFn = (route, state) => {
-  return true;
+  const router = inject(Router);
+
+  try {
+    const token = localStorage.getItem('ACCESS_TOKEN');
+    if (token) {
+      const payload: TokenPayload = jwtDecode(token);
+      if (payload.roles.includes('admin')) {
+        return true;
+      }
+    }
+  } catch (error) {
+    console.error('Error decoding token', error);
+  }
+
+  router.navigate(['/auth/login']);
+  return false;
 };
