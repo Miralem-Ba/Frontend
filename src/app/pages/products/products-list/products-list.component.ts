0739b113d0802a1e0bbbb42a48647d523667eac6
Miralem.Ba <miralembanovi@gmail.com>
2023-12-16 11:20:40 +0100

    products-list.component.ts bearbeitet

--- src/app/pages/products/products-list/products-list.component.ts
+++ src/app/pages/products/products-list/products-list.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatTableModule} from "@angular/material/table";
+import {ProductControllerService, ProductShowDto} from "../../../openapi-client";
 
 @Component({
   selector: 'pm-products-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatTableModule],
   templateUrl: './products-list.component.html',
   styleUrl: './products-list.component.scss'
 })
 export class ProductsListComponent {
+  products: Array<ProductShowDto> = [];
+  columnDefinition: string[] = ['name','id','price','stock','image','sku'];
 
+  constructor(private  readonly productControllerService: ProductControllerService) {
+    this.productControllerService.getAllProducts().subscribe(products => {
+      this.products = products;
+    })
+  }
 }
