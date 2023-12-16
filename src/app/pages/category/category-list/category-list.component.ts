cc0ecfbe64dadfce325b729d600ff9dbd9a07819
Miralem.Ba <miralembanovi@gmail.com>
2023-12-16 11:19:18 +0100

    category-list.component erstellt und bearbeitet

--- src/app/pages/category/category-list/category-list.component.ts
+++ src/app/pages/category/category-list/category-list.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {CategoryControllerService, CategoryShowDto} from "../../../openapi-client";
+import {MatIconModule} from "@angular/material/icon";
+import {MatTableModule} from "@angular/material/table";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'pm-category-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule, MatTableModule, MatButtonModule],
   templateUrl: './category-list.component.html',
   styleUrl: './category-list.component.scss'
 })
 export class CategoryListComponent {
-
+  columnNames: string[] = ['name', 'id','action'];
+  allCategories: CategoryShowDto[] = [];
+  constructor(
+    private categoryControllerService: CategoryControllerService){
+    this.categoryControllerService.getAllCategories().subscribe(categories => {
+      this.allCategories = categories;
+    })
+    }
 }
