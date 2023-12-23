984ac92f476532eedbaf890fe4260f3f513d418b
Miralem.Ba <miralembanovi@gmail.com>
2023-12-23 17:24:09 +0100

    category-list.component überarbeitet

--- src/app/pages/category/category-list/category-list.component.ts
+++ src/app/pages/category/category-list/category-list.component.ts
@@ -1,86 +1,74 @@
-import {Component, OnInit, signal} from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CategoryControllerService, CategoryShowDto } from "../../../openapi-client";
+import Swal from 'sweetalert2';
 import { MatIconModule } from "@angular/material/icon";
 import { MatTableModule } from "@angular/material/table";
 import { MatButtonModule } from "@angular/material/button";
-import { Router } from '@angular/router';
-import Swal from 'sweetalert2';
-
-import {
-  CategoryControllerService,
-  CategoryShowDto,
-  CategoryUpdateDto
-} from "../../../openapi-client";
 
 @Component({
   selector: 'pm-category-list',
   standalone: true,
-  imports: [CommonModule, MatIconModule, MatTableModule, MatButtonModule],
+  imports: [
+    MatIconModule,
+    MatTableModule,
+    MatButtonModule,
+  ],
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.scss']
 })
 export class CategoryListComponent implements OnInit {
-  columnNames: string[] = ['name', 'id', 'action'];
   allCategories: CategoryShowDto[] = [];
-  onCreate = signal<any | null>(null);
+  columnNames: string[] = ['name', 'id', 'action'];
 
   constructor(
     private categoryControllerService: CategoryControllerService,
     private router: Router
   ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.getAllCategories();
   }
 
   getAllCategories(): void {
-    this.categoryControllerService.getAllCategories().subscribe(categories => {
-      this.allCategories = categories;
-    }, error => {
-      console.error('Error fetching categories:', error);
+    this.categoryControllerService.getAllCategories().subscribe({
+      next: (categories) => {
+        this.allCategories = categories;
+      },
+      error: (error) => {
+        console.error('Error fetching categories:', error);
+        Swal.fire('Error!', 'There was a problem fetching categories.', 'error');
+      }
     });
-  }
-
-  createCategory(): void {
-    this.router.navigate(['/category/create']);
   }
 
   editCategory(categoryId: number): void {
     this.router.navigate(['/category/edit', categoryId]);
   }
 
-  updateCategory(category: CategoryShowDto): void {
-    const updatePayload: CategoryUpdateDto = {
-      // Sie müssen die Eigenschaften von CategoryShowDto auf CategoryUpdateDto abbilden
-      name: category.name,
-      active: true // Dies ist ein Beispielwert
-    };
-
-    this.categoryControllerService.updateCategoryById(category.id, updatePayload).subscribe(() => {
-      Swal.fire('Success', 'Category updated successfully', 'success');
-      this.getAllCategories();
-    }, error => {
-      console.error('Error updating category:', error);
-    });
-  }
-
   deleteCategory(categoryId: number): void {
     Swal.fire({
       title: 'Are you sure?',
-      text: 'This action cannot be undone!',
+      text: "You won't be able to revert this!",
       icon: 'warning',
       showCancelButton: true,
-      confirmButtonText: 'Yes, delete it!',
-      cancelButtonText: 'No, cancel'
+      confirmButtonText: 'Yes, delete it!'
     }).then((result) => {
-      if (result.value) {
-        this.categoryControllerService.deleteCategoryById(categoryId).subscribe(() => {
-          Swal.fire('Deleted!', 'Category has been deleted.', 'success');
-          this.getAllCategories();
-        }, error => {
-          console.error('Error deleting category:', error);
+      if (result.isConfirmed) {
+        this.categoryControllerService.deleteCategoryById(categoryId).subscribe({
+          next: () => {
+            this.allCategories = this.allCategories.filter(category => category.id !== categoryId);
+            Swal.fire('Deleted!', 'Your category has been deleted.', 'success');
+          },
+          error: () => {
+            Swal.fire('Error!', 'There was an error deleting the category.', 'error');
+          }
         });
       }
     });
+  }
+
+  onCreate(): void {
+    this.router.navigate(['/category/create']);
   }
 }
